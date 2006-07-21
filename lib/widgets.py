4d97656422c921cb80a3179e8dc763a9698bd3b1
Piotr Ożarowski <piotr@griffith.cc>
2006-07-21 07:06:17 +0000

    Updated Free Software Foundation address

--- lib/widgets.py
+++ lib/widgets.py
@@ -16,7 +16,7 @@ __revision__ = '$Id$'
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 # You may use and distribute this software under the terms of the
 # GNU General Public License, version 2 or later
