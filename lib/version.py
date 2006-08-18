3716ba6ce2110616e05f9dfdbf935c5a81b57e97
Piotr Ożarowski <piotr@griffith.cc>
2006-08-18 11:25:14 +0000

    version changed to 0.6.2+svn

--- lib/version.py
+++ lib/version.py
@@ -23,7 +23,7 @@ __revision__ = '$Id$'
 
 from gettext import gettext as _
 pname        = "Griffith"
-pversion     = "0.6.1+svn"
+pversion     = "0.6.2+svn"
 pauthor      = "Vasco Nunes <vasco.m.nunes@gmail.com>"
 pyear        = "2005-2006"
 pwebsite     = "http://griffith.vasconunes.net/"
