700cb5e8ef5e59db014af88a2f2ecd932f985200
Piotr Ożarowski <piotr@griffith.cc>
2006-12-30 20:49:01 +0000

    Add Jessica's icon for seen/unseen movies

--- lib/main_treeview.py
+++ lib/main_treeview.py
@@ -137,9 +137,9 @@ def set_details(self, item=None):#{{{
 		self._trailer_url = None
 		w['go_trailer_button'].set_sensitive(False)
 	if item.has_key('seen') and item['seen'] == True:
-		w['seen_icon'].set_from_stock('gtk-yes', 2)
+		w['seen_icon'].set_from_file(os.path.join(self.locations['images'], 'seen.png'))
 	else:
-		w['seen_icon'].set_from_stock('gtk-no', 2)
+		w['seen_icon'].set_from_file(os.path.join(self.locations['images'], 'unseen.png'))
 	if item.has_key('notes') and item['notes']:
 		w['notes'].set_text(str(item.notes))
 	else:
