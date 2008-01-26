0f22cf8cfcf24f25b02183c05371370359be9b5e
Piotr Ożarowski <piotr@griffith.cc>
2008-01-26 20:01:20 +0000

    fill in translations tab in about dialog automatically using TRANSLATORS file

--- lib/about.py
+++ lib/about.py
@@ -29,7 +29,9 @@ import sys
 
 class AboutDialog:
 	"""Shows a gtk about dialog"""
-	def __init__(self, images_dir):
+	def __init__(self, locations):
+		TRANSLATORS_FILE = os.path.join(locations['share'], 'TRANSLATORS') # remember to encode this file in UTF-8
+		IMAGES_DIR = locations['images']
 		dialog = gtk.AboutDialog()
 		dialog.set_name(version.pname)
 		dialog.set_version(version.pversion)
@@ -50,65 +52,24 @@ class AboutDialog:
 			"\nPlease visit http://www.peekmambo.com/\n"),
 			'seen / unseen icons by dragonskulle <dragonskulle@gmail.com>'
 		])
-		dialog.set_translator_credits(\
-			_("Brasilian Portuguese") + ":\n\t" + \
-				"Fábio Nogueira <deb-user-ba@ubuntu.com>\n\t" + \
-				"Alan A. Dantas <alan.arsolto@gmail.com>\n\t" + \
-				"Augusta Carla Klug <augusta_klug@yahoo.com.br>\n\t" + \
-				"alexandrers <alexandrenescau@gmail.com>\n" + \
-			_("Bulgarian") + ":\n\t" + \
-				"Luchezar P. Petkov <luchezar.petkov@gmail.com>\n" + \
-			_("Catalan") + ":\n\t" + \
-				"el_libre <el.libre@gmail.com>\n" + \
-			_("Czech") + ":\n\t" + \
-				"Blondak <blondak@neser.cz>,\n\t" + \
-				"Ondra 'Kepi' Kudlík <kepi@igloonet.cz>\n\t" + \
-				"Kamil Páral <ripper42@gmail.com>\n" + \
-			_("Danish") + ":\n\t" + \
-				"Joe Dalton <joedalton2@yahoo.dk>\n" + \
-			_("Dutch") + ":\n\t" + \
-				"Marcel Dijkstra <mdtje@hotmail.com>\n\t" + \
-				"Tominator <lambik+launchpad@gmail.com>\n\t" + \
-				"warddr <ward.ubuntu@gmail.com>\n" + \
-			_("French") + ":\n\t" + \
-				"Guillaume Pratte <guillaume@guillaumepratte.net>\n\t" + \
-				"Pierre-Luc Lévy <pllevy@free.fr>\n\t" + \
-				"antou <antoou+inscriptions@gmail.com>\n\t" + \
-				"Rémi Preghenella <remi.pregh@gmail.com>\n\t" + \
-				"sd2310 <sd2310@gmail.com>\n" + \
-			_("German") + ":\n\t" + \
-				"Jessica Katharina Parth <Jessica.K.P@women-at-work.org>,\n\t" + \
-				"Sebastian Wallroth <sebastian@wallroth.de>\n\t" + \
-				"Christian Sagmueller <christian@sagmueller.net>,\n\t" + \
-				"Malte Wiemann <ryan2057@gmx.de>\n" + \
-			_("Greek") + ":\n\t" + \
-				"Ioannis Koniaris <ikoniari@csd.auth.gr>\n\t" + \
-				"Athanasia Tziola <erynies@gmail.com>\n\t" + \
-				"linuxangel <chraniotis@gmail.com>\n" + \
-			_("Italian") + ":\n\t" + \
-				"Diego Porcelli <diego.p77@gmail.com>\n\t" + \
-				"Simone Vendemia <simonevendemia@gmail.com>\n" + \
-			_("Japanese") + ":\n\t" + \
-				"Jack Nihil <jnihil@gmail.com>\n" + \
-			_("Norwegian Bokmal") + ":\n\t" + \
-				"Anders Oftedal <anders.oftedal@gmail.com>\n" + \
-			_("Polish") + ":\n\t" + \
-				"Piotr Ozarowski <ozarow+griffith@gmail.com>\n" + \
-			_("Portuguese") + ":\n\t" + \
-				"Vasco Nunes <vasco.m.nunes@gmail.com>\n" + \
-			_("Russian") + ":\n\t" + \
-				"Pavel V. Kulikov <kulikovpv8256@yandex.ru>\n\t" + \
-				"Nkolay Parukhin <nik@sevpinro.ru>\n" + \
-			_("Simplified Chinese") + ":\n\t" + \
-				"kempson <kempson.chen@gmail.com>\n" + \
-			_("Spanish") + ":\n\t" + \
-				"Daniel Ucero <escaranbujo@gmail.com>\n" + \
-			_("Swedish") + ":\n\t" + \
-				"Daniel Nylander <po@danielnylander.se>\n" + \
-			_("Turkish") + ":\n\t" + \
-				"transorlate <iloveshorts@hotmail.com>\n" \
-		)
-		logo_file = os.path.abspath(os.path.join(images_dir, 'griffith.png'))
+		data = None
+		if os.path.isfile(TRANSLATORS_FILE):
+			data = open(TRANSLATORS_FILE).read()
+		elif os.path.isfile(TRANSLATORS_FILE+'.gz'):
+			from gutils import decompress
+			data = decompress(open(TRANSLATORS_FILE + '.gz').read())
+		translator_credits = ''
+		if data:
+			for line in data.split('\n'):
+				if line.startswith('* '):
+					lang = line[2:]
+					if _(lang) != lang:
+						line = "* %s:" % _(lang)
+				translator_credits += "%s\n" % line
+		else:
+			translator_credits = _("See TRANSLATORS file")
+		dialog.set_translator_credits(translator_credits)
+		logo_file = os.path.abspath(os.path.join(IMAGES_DIR, 'griffith.png'))
 		logo = gtk.gdk.pixbuf_new_from_file(logo_file)
 		dialog.set_logo(logo)
 		if os.path.isfile('/usr/share/common-licenses/GPL-2'):
